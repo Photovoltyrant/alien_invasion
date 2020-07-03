fa72ebcf2340e49c604006f51c06501dff57a83c
photovoltyrant <63938015+Photovoltyrant@users.noreply.github.com>
2020-07-03 17:58:02 -0400

    progress through 14.3

--- settings.py
+++ settings.py
@@ -14,7 +14,7 @@ class Settings():
         self.ship_limit = 3
         
         # Bullet settings
-        self.bullet_width = 3
+        self.bullet_width = 300
         self.bullet_height = 15
         self.bullet_color = 60, 60, 60
         self.bullets_allowed = 10
@@ -24,6 +24,9 @@ class Settings():
         
         # How quickly the game speeds up
         self.speedup_scale = 1.1
+        
+        # How quickly the alien point values increases
+        self.score_scale = 1.5
         
         self.initialize_dynamic_settings()
         
@@ -36,8 +39,13 @@ class Settings():
         # flee_direction of 1 represents right; -1 represents left.
         self.fleet_direction = 1
         
+        # Scoring
+        self.alien_points = 50
+        
     def increase_speed(self):
-        """Increase speed settings."""
+        """Increase speed settings and alien point values."""
         self.ship_speed_factor *= self.speedup_scale
         self.bullet_speed_factor *= self.speedup_scale
         self.alien_speed_factor *= self.speedup_scale
+        
+        self.alien_points = int(self.alien_points * self.score_scale)
--- ship.py
+++ ship.py
@@ -1,11 +1,13 @@
 """This module sets ship settings"""
 
 import pygame
+from pygame.sprite import Sprite
 
-class Ship():
+class Ship(Sprite):
     
     def __init__(self, ai_settings, screen):
         """Initialize the ship and set its starting point."""
+        super(Ship, self).__init__()
         self.screen = screen
         self.ai_settings = ai_settings
         
